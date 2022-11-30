80f12aa206b323e17839a9b639cb8a01edb05150
Saparmurat <saparmurat@protonmail.com>
2022-12-01 01:27:00 +0600

    add models for user app

--- user/models.py
+++ user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    surname = models.CharField(max_length=100, blank=False)
+
+    email = models.EmailField(max_length=256)
+    phone = models.CharField(max_length=100, blank=False)
+
+    is_admin = models.BooleanField(default=False)
+
+    birth_date = models.DateField()
+
+    # TODO add clubcard creation
+
+
+class ClubCard(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+
+    spent = models.FloatField(default=0)
+    discount = models.FloatField(default=0)
