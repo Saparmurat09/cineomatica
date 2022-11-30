4a529382835eedc74d520fbd0be75d43c1fe09a6
Saparmurat <saparmurat@protonmail.com>
2022-12-01 01:51:26 +0600

    add models for app booking

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from cinema.models import Movie, Room, Seat
+from user.models import User
 
-# Create your models here.
+class Session(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    start_time = models.TimeField(blank=False)
+    end_time = models.TimeField(blank=False)
+
+
+class Pricing(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.DO_NOTHING)
+    
+    children = models.FloatField(default=0)    
+    adult = models.FloatField(default=0)    
+    student = models.FloatField(default=0)    
+    
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    time = models.TimeField(auto_created=True)
+
+    total_price = models.FloatField(default=0)    
+
+
+class Ticket(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.DO_NOTHING)
+    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    seat = models.ForeignKey(Seat, on_delete=models.DO_NOTHING)
+
+    CATEGORY = (
+        (1, "Children"),
+        (2, "Student"),
+        (3, "Adult"),
+    )
+
+    category = models.Choices(CATEGORY)
+    count = models.IntegerField(default=1)
