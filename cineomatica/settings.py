03bc7e8b83058bcaadfe7811d27ebf6b933c5ce8
Saparmurat <saparmurat@protonmail.com>
2022-11-30 23:26:20 +0600

    add installed apps

--- cineomatica/settings.py
+++ cineomatica/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booking',
+    'cinema',
+    'user',
 ]
 
 MIDDLEWARE = [
