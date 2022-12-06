e3d55418067db9c5aa71482eaf0ec423be0c7e36
Saparmurat <saparmurat@protonmail.com>
2022-12-06 14:43:15 +0600

    user: setup usermanager

--- user/models.py
+++ user/models.py
@@ -1,18 +1,50 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email is required field')
+        
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('superuser must have is_superuser = True') 
+
+        user = self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     name = models.CharField(max_length=100, blank=False)
     surname = models.CharField(max_length=100, blank=False)
 
-    email = models.EmailField(max_length=256)
+    email = models.EmailField(max_length=256, blank=False, unique=True)
     phone = models.CharField(max_length=100, blank=False)
 
     is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     birth_date = models.DateField(blank=True, null=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'surname']
 
+    objects = UserManager()
+
+    def __str__(self):
+        return self.name
 
 
 class ClubCard(models.Model):
