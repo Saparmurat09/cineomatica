6a5b600fab772d1d28cf000538d75014f4e5bbec
Saparmurat <saparmurat@protonmail.com>
2022-12-07 12:12:50 +0600

    cinema: setup urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,23 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import (
+    MovieView,
+    CinemaView,
+    RoomView,
+    ContactView,
+    AddressView,
+)
+
+router = routers.SimpleRouter()
+
+router.register('movie', MovieView)
+router.register('cinema', CinemaView)
+router.register('room', RoomView)
+router.register('contact', ContactView)
+router.register('address', AddressView)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
