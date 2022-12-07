9b4be776530312176854f71fcb5afa13ac528ae1
Saparmurat <saparmurat@protonmail.com>
2022-12-07 12:13:36 +0600

    cinema: create views

--- cinema/views.py
+++ cinema/views.py
@@ -1,2 +1,42 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import (
+   Movie,
+   Cinema,
+   Room,
+   Feedback,
+   Address,
+   Contact, 
+)
 
+from .serializers import (
+    MovieSerializer,
+    CinemaSerializer,
+    RoomSerializer,
+    FeedbackSerializer,
+    AddressSerializer,
+    ContactSerializer,
+)
+
+class MovieView(ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class CinemaView(ModelViewSet):
+    queryset = Cinema.objects.all()
+    serializer_class = CinemaSerializer
+
+
+class RoomView(ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class ContactView(ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+class AddressView(ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer()
