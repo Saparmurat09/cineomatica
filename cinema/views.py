227ce75f7dd98b2c89c17f72de9e3bb432f4ae8c
Saparmurat <saparmurat@protonmail.com>
2022-12-09 18:17:45 +0600

    views: add CreateSeatView

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
+
 from .models import (
    Movie,
    Cinema,
@@ -6,6 +8,7 @@ from .models import (
    Feedback,
    Address,
    Contact, 
+   Seat,
 )
 
 from .serializers import (
@@ -15,6 +18,8 @@ from .serializers import (
     FeedbackSerializer,
     AddressSerializer,
     ContactSerializer,
+    CreateSeatSerializer,
+    SeatSerializer,
 )
 
 class MovieView(ModelViewSet):
@@ -39,4 +44,28 @@ class ContactView(ModelViewSet):
 
 class AddressView(ModelViewSet):
     queryset = Address.objects.all()
-    serializer_class = AddressSerializer()
+    serializer_class = AddressSerializer
+
+
+class CreateSeatView(ModelViewSet):
+    queryset = Seat.objects.all()
+
+    def perform_create(self, serializer):
+        room = serializer.validated_data['room']
+        seats = serializer.validated_data['seats'] 
+
+        room = Room.objects.get(id=room)
+
+        # TODO check for repeating seat row, columns
+
+        for i in range(len(seats)):
+            row = i + 1
+            for j in range(seats[i]):
+                column = j + 1
+                seat = Seat.objects.create(room=room, column=column, row=row)
+                seat.save()
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return SeatSerializer
+        return CreateSeatSerializer
