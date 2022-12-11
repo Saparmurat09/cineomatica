31aae68869255f50e57553a99c4a2ca412eb77a4
Saparmurat <saparmurat@protonmail.com>
2022-12-12 00:16:53 +0600

    test: write test for homepage

--- cinema/tests.py
+++ cinema/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from rest_framework.reverse import reverse
+# from django.urls import reverse
 
-# Create your tests here.
+
+class TestHomePage(APITestCase):
+    def test_homepage(self):
+        url = reverse('root')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class TestMovie():
+    def test_get():
+        pass
