1f9b88d9296b301d4cd850ed4096d30ab20eb50d
Saparmurat <saparmurat@protonmail.com>
2022-12-12 22:46:06 +0600

    urls: use plural names

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from rest_framework import routers
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+
+from rest_framework_nested import routers
 
 from .views import (
     MovieView,
@@ -18,27 +20,34 @@ from .views import (
 
 router = routers.SimpleRouter()
 
-router.register('movie', MovieView, basename='movie')
-router.register('cinema', CinemaView, basename='cinema')
-router.register('room', RoomView, basename='room')
-router.register('contact', ContactView, basename='contact')
-router.register('address', AddressView, basename='address')
-router.register('session', SessionView, basename='session')
-router.register('pricing', PricingView, basename='pricing')
-router.register('feedback', FeedbackView, basename='feedback')
+router.register('movies', MovieView, basename='movie')
+
+movies_router = routers.NestedSimpleRouter(router, 'movies', lookup='movie')
+movies_router.register('sessions', SessionView, basename='movies-sessions')
+
+
+router.register('cinemas', CinemaView, basename='cinema')
+router.register('rooms', RoomView, basename='room')
+router.register('contacts', ContactView, basename='contact')
+router.register('addresss', AddressView, basename='address')
+router.register('sessions', SessionView, basename='session')
+router.register('pricings', PricingView, basename='pricing')
+router.register('feedbacks', FeedbackView, basename='feedback')
 
 
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
-        'movie': reverse('movie-list', request=request, format=format),
-        'cinema': reverse('cinema-list', request=request, format=format),
-        'room': reverse('room-list', request=request, format=format),
-        'session': reverse('session-list', request=request, format=format),
+        'movies': reverse('movie-list', request=request, format=format),
+        'cinemas': reverse('cinema-list', request=request, format=format),
+        'rooms': reverse('room-list', request=request, format=format),
+        'sessions': reverse('session-list', request=request, format=format),
+        'feedbacks': reverse('feedback-list', request=request, format=format),
     })
 
 urlpatterns = [
     path('', api_root, name='root'),
     path('', include(router.urls)),
+    path('', include(movies_router.urls)),
     path('seat/', CreateSeatView.as_view({'get': 'list', 'post': 'create'}), name='seat'),
 ]
