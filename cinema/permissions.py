87567cc5e4d1e26bd655d04389f239083b4b668b
Saparmurat <saparmurat@protonmail.com>
2022-12-12 22:44:14 +0600

    permissions: add owner permissions

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -7,4 +7,13 @@ class IsAdminOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
 
-        return bool(request.user and request.user.is_staff)
+        return bool(request.user and request.user.is_staff or request.user.is_admin)
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.user == request.user
+        
