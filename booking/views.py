edd2c1c0c470df0162407f45be983fe5f1276e6c
Saparmurat <saparmurat@protonmail.com>
2022-12-14 02:06:02 +0600

    views: create tickets

--- booking/views.py
+++ booking/views.py
@@ -6,6 +6,8 @@ from rest_framework.exceptions import ValidationError
 
 from .models import Ticket, Order
 
+from cinema.models import Session, Room, Seat, Pricing
+
 from .serializers import TicketSerializer, OrderSerializer, CreateTicketSerializer
 
 class TicketView(viewsets.ModelViewSet):
@@ -17,19 +19,76 @@ class TicketView(viewsets.ModelViewSet):
             return Ticket.objects.filter(user=user)
         return Ticket.objects.all()
 
-    def create(self, request):
+    def create(self, request, *args, **kwargs):
         user = request.user
-        data = request.data
-    
-        # if not 'seats' in data or not 'session' in data:
-        #     return Response(data, status=status.HTTP_400_BAD_REQUEST)
-            # raise ValidationError('data')
-        
-        # order = Order.objects.create(user=user)
-        # print(user)
-        # print(data['seats'])
+        try:
+            session = request.data['session']
+            seats = request.data['seats']
+        except:
+            raise ValidationError({"Invalid data": request.data})
 
-        return Response(data, status=status.HTTP_201_CREATED)
+        if len(seats) > 6:
+            raise ValidationError({'Tickets': 'Can book only 6 tickets for a session'})
+
+        try :
+            session = Session.objects.get(id=session)
+        except Session.DoesNotExist:
+            raise ValidationError({'Session': 'Session does not exist'})
+
+        data = []
+
+        for seat in seats:
+            try:
+                row = seat['row']
+                column = seat['column']
+                category = seat['category'] 
+            except:
+                raise ValidationError({"Invalid data": seats})
+
+            print(seat)
+
+            try:
+                st = Seat.objects.get(row=row, column=column, room=session.room)
+            except Seat.DoesNotExist:
+                raise ValidationError({'Seat': 'Seat does not exist'})
+
+            if Ticket.objects.filter(seat=st, session=session).exists():
+                raise ValidationError({
+                    'Seat': 'Seat is already booked for this session',
+                    'Data': seat
+                })
+            
+            data.append({
+                'user': user.id,
+                'session': session.id,
+                'seat': st.id,
+                'category': category
+            })
+
+        pricing = Pricing.objects.get(session=session)
+
+        order = Order.objects.create(user=user)
+
+        ret = {
+            'Tickets': []
+        }
+
+        for record in data:
+            record['order'] = order.id
+
+            serializer = TicketSerializer(data=record, context={'request': request})
+
+            if serializer.is_valid():
+                serializer.save(user=user, order=order)
+                serializer.save()
+                ret['Tickets'].append(serializer.data)
+            else:
+                raise ValidationError({
+                    'Ticket': 'Invalid data',
+                    'Data': record
+                })
+
+        return Response(ret, status=status.HTTP_201_CREATED)
 
 
     def get_serializer_class(self):
